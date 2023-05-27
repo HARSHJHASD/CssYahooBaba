228ba4d6f4222b9d08716496f892e2e6578b1324
Harsh Jha <harshjha19101997@gmail.com>
2023-05-27 02:16:29 -0700

    Completed In 20 Mints

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <div class="outerbox">
+          <div class="firstDiv">
+            <h1 className='topHeading'>
+              Website Name
+            </h1>
+          </div>
+          <div className='navbar'>
+            <span className='eachNavbar'>
+              Home
+            </span>
+            <span className='eachNavbar'>
+              About Us
+            </span>
+            <span className='eachNavbar'>
+              Gallery
+            </span>
+            <span className='eachNavbar'>
+              Contact Us
+            </span>
+            <span className='eachNavbar'>
+              Home
+            </span>
+          </div>
+          <div className='mainDiv'>
+            <div className='leftSide'>
+              <h2>
+                Sub Heading
+              </h2>
+              <div className='textBox'>
+                <p>
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <p>
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently.
+                </p>
+              </div>
+            </div>
+            <div className='rightSide'>
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Gallery</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+          </div>
+          <div className='footer'>
+            <p className='footerText'>Website Name@Copyright</p>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
-
 export default App;
